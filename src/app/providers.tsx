9770d30389051719df646943561f59e5355f77a2
Efen Stakes <efenstakes101@gmail.com>
2023-09-28 16:46:02 +0300

    add providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ThemeProvider } from '@mui/material';
+import theme from '@/styles/theme';
+
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default Providers
