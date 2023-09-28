6e5d64b82bbab269c4a62b509a53c69bf72ed0ec
Efen Stakes <efenstakes101@gmail.com>
2023-09-28 17:49:57 +0300

    add site sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,13 @@ import { useWindowWidth } from '@react-hook/window-size';
 import Appbar from "@/components/appbar/component"
 import VSpacer from "@/components/v_spacer/component"
 import Welcome from "@/components/welcome/component"
+import CompaniesSection from "@/components/companies_section/component";
 
 import './page.scss'
+import ProjectsSection from "@/components/projects_section/component";
+import SectionTitle from "@/components/section_title/component";
+import SkillsSection from "@/components/skills_section/component";
+import ContactSection from "@/components/contact_section/component";
 
 export default function Home() {
   const width = useWindowWidth()
@@ -34,6 +39,27 @@ export default function Home() {
         <Welcome scrollToCompanies={scrollToCompanies} />
         <VSpacer space={8} />
         
+        {/* companies */}
+        <div className='companies__scroll_to' ref={companiesScrollToRef} />
+        <CompaniesSection />
+        <VSpacer space={8} />
+
+        {/* projects */}
+        <ProjectsSection />
+        <VSpacer space={8} />
+
+        <SectionTitle title="SKILLS" />
+        <VSpacer space={2} />
+        <SkillsSection />
+        <VSpacer space={8} />
+
+        <div className="contact_section">
+            <SectionTitle title="CONTACT ME" isWhite />
+            <VSpacer space={2} />
+            <ContactSection />
+        </div>
+        <VSpacer space={2} />
+
       </div>
     </div>
   )
