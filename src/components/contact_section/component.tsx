3ee79c65cfafe472a420a70eacea36d0f9495914
Efen Stakes <efenstakes101@gmail.com>
2025-02-11 18:34:37 +0300

    update portfolio links

--- src/components/contact_section/component.tsx
+++ src/components/contact_section/component.tsx
@@ -2,7 +2,9 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import AttachEmailIcon from '@mui/icons-material/AttachEmail'
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
+import { FaMedium } from "react-icons/fa6";
+import { FaLinkedin } from 'react-icons/fa';
+
 
 
 // components
@@ -15,23 +17,27 @@ const ContactSection = () => {
         <div className="contact">
             
 
-            <a href='https://github.com/efenstakes' className="contact_card su_12" target='_blank'>
+            <a href='https://github.com/efenstakes' title="My Github Link" className="contact_card su_12" target='_blank'>
                 <GitHubIcon />
-                <p> Efenstakes Git </p>
+                <p> Github </p>
             </a>
-            <a href='https://www.linkedin.com/in/felix-ndunda-0ba841108/' className="contact_card su_14" target='_blank'>
-                <LinkedInIcon />
-                <p> Felix Ndunda </p>
+            
+
+            <a href='https://medium.com/@efenstakes101' title="Medium personal blog" className="contact_card su_12" target='_blank'>
+                <FaMedium />
+                <p> Medium Blog </p>
             </a>
 
-            <a href='mailto:efenstakes101@gmail.com' className="contact_card su_15">
+
+            <a href='https://www.linkedin.com/in/felix-ndunda-0ba841108/' title="LinkedIn link" className="contact_card su_12" target='_blank'>
+                <FaLinkedin />
+                <p> LinkedIn </p>
+            </a>
+
+
+            <a href='mailto:efenstakes101@gmail.com' title="Email me" className="contact_card su_15">
                 <AttachEmailIcon />
                 <p> efenstakes101@gmail.com </p>
-            </a>
-
-            <a href='tel:+254714335322' className="contact_card su_16">    
-                <LocalPhoneIcon />
-                <p> +254-799-91-9960 </p>
             </a>
             
         </div>
