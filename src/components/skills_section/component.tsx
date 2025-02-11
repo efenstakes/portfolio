19d3a27d62a18f1c3d731d9d16b1516f9fdf3eb6
Efen Stakes <efenstakes101@gmail.com>
2025-02-11 18:33:19 +0300

    update my skills

--- src/components/skills_section/component.tsx
+++ src/components/skills_section/component.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-import { Box, CircularProgress, CircularProgressProps, LinearProgress, Typography } from '@mui/material'
-
+// import { Box, CircularProgress, CircularProgressProps, } from '@mui/material'
+import Box from '@mui/material/Box'
+import CircularProgress, { CircularProgressProps, } from '@mui/material/CircularProgress'
 import { SiSolidity, SiRust, SiElixir, SiTypescript, SiBabylondotjs, SiSwift, SiFlutter, SiDocker, SiFirebase, SiChai, SiMocha, SiGraphql, SiWebrtc, } from 'react-icons/si'
 import { FaHardHat, FaAws, FaGitAlt, } from 'react-icons/fa'
 import { BiAnchor, BiLogoPython, BiLogoReact, BiLogoJavascript, BiLogoCss3, } from 'react-icons/bi'
-import { TbBrandGolang, TbBrandNodejs, TbBrandNextjs, TbBrandThreejs, TbBrandReactNative, } from 'react-icons/tb'
+import { TbBrandGolang, TbBrandNodejs, TbBrandNextjs, TbBrandThreejs, TbBrandReactNative, TbPrompt, } from 'react-icons/tb'
 import { AiFillHtml5, } from 'react-icons/ai'
 
 // models
 import { Skill } from '../../models/skill'
-
 
 // components
 import VSpacer from '../v_spacer/component'
@@ -20,7 +20,6 @@ const frontend_img = '/assets/images/fr3.png'
 const block_img = '/assets/images/block.jpg'
 const mobile_img = '/assets/images/mobile.jpeg'
 const devops_img = '/assets/images/devops.jpg'
-
 
 // styles
 import './component.scss'
@@ -54,47 +53,50 @@ function CircularProgressWithLabel(
 const SkillsSection = () => {
 
     const toolIcons = {
-         "Solidity": <SiSolidity className='skill_icon' />,
-         "Rust": <SiRust className='skill_icon' /> ,
-         "HardHat": <FaHardHat className='skill_icon' /> ,
-         "Anchor": <BiAnchor className='skill_icon' /> ,
-         "Go": <TbBrandGolang className='skill_icon' />,
-         "NodeJs": <TbBrandNodejs className='skill_icon' /> ,
-         "Elixir": <SiElixir  className='skill_icon'/> ,
-         "Python": <BiLogoPython className='skill_icon' /> ,
-         "NextJS": <TbBrandNextjs className='skill_icon' /> ,
-         "React": <BiLogoReact className='skill_icon' /> ,
-         "Material UI": <BiLogoReact className='skill_icon' /> ,
-         "Jest, Chai": <SiChai className='skill_icon'/> ,
-         "Mocha": <SiMocha className='skill_icon' /> ,
-         "Jest": <SiMocha className='skill_icon' /> ,
-         "GraphQL": <SiGraphql className='skill_icon' /> ,
-         "Webrtc": <SiWebrtc className='skill_icon' /> ,
-         "TypeScript": <SiTypescript className='skill_icon' /> ,
-         "Javascript": <BiLogoJavascript className='skill_icon' /> ,
-         "HTML5": <AiFillHtml5 className='skill_icon' /> ,
-         "CSS3": <BiLogoCss3 className='skill_icon' /> ,
-         "WegGL": <TbBrandThreejs className='skill_icon' /> ,
-         "Three JS": <TbBrandThreejs className='skill_icon' /> ,
-         "Babylon JS": <SiBabylondotjs className='skill_icon' /> ,
-         "Flutter": <SiFlutter className='skill_icon' /> ,
-         "React Native": <TbBrandReactNative className='skill_icon' /> ,
-         "Swift": <SiSwift className='skill_icon' /> ,
-         "SwiftUI": <SiSwift className='skill_icon' /> ,
-         "Docker": <SiDocker className='skill_icon' /> ,
-         "AWS": <FaAws className='skill_icon' /> ,
-         "Git": <FaGitAlt className='skill_icon' /> ,
-         "Firebase": <SiFirebase className='skill_icon' />,
+        "Solidity": <SiSolidity className='skill_icon' />,
+        "Rust": <SiRust className='skill_icon' /> ,
+        "HardHat": <FaHardHat className='skill_icon' /> ,
+        "Anchor": <BiAnchor className='skill_icon' /> ,
+        "Go": <TbBrandGolang className='skill_icon' />,
+        "NodeJs": <TbBrandNodejs className='skill_icon' /> ,
+        "Elixir": <SiElixir  className='skill_icon'/> ,
+        "Python": <BiLogoPython className='skill_icon' /> ,
+        "NextJS": <TbBrandNextjs className='skill_icon' /> ,
+        "React": <BiLogoReact className='skill_icon' /> ,
+        "Material UI": <BiLogoReact className='skill_icon' /> ,
+        "Jest, Chai": <SiChai className='skill_icon'/> ,
+        "Mocha": <SiMocha className='skill_icon' /> ,
+        "Jest": <SiMocha className='skill_icon' /> ,
+        "GraphQL": <SiGraphql className='skill_icon' /> ,
+        "Webrtc": <SiWebrtc className='skill_icon' /> ,
+        "TypeScript": <SiTypescript className='skill_icon' /> ,
+        "Javascript": <BiLogoJavascript className='skill_icon' /> ,
+        "HTML5": <AiFillHtml5 className='skill_icon' /> ,
+        "CSS3": <BiLogoCss3 className='skill_icon' /> ,
+        "HTML5 & CSS3": <AiFillHtml5 className='skill_icon' /> ,
+        "WegGL": <TbBrandThreejs className='skill_icon' /> ,
+        "Three JS": <TbBrandThreejs className='skill_icon' /> ,
+        "Babylon JS": <SiBabylondotjs className='skill_icon' /> ,
+        "Flutter": <SiFlutter className='skill_icon' /> ,
+        "React Native": <TbBrandReactNative className='skill_icon' /> ,
+        "Swift": <SiSwift className='skill_icon' /> ,
+        "SwiftUI": <SiSwift className='skill_icon' /> ,
+        "Docker": <SiDocker className='skill_icon' /> ,
+        "AWS": <FaAws className='skill_icon' /> ,
+        "Git": <FaGitAlt className='skill_icon' /> ,
+        "Firebase": <SiFirebase className='skill_icon' />,
+        //  "Langchain": <SiChainlink className='skill_icon' />,
+        //  "Prompt Engineering": <TbPrompt className='skill_icon' />
     }
     const skills: Array<Skill> = [
         {
-            title: "Blockchain",
-            image: block_img,
+            title: "AI & Machine Learning",
+            image: backend_img,
             tools: [
-                { tool: "Solidity", proeffiency: 98 },
-                { tool: "Rust", proeffiency: 84 },
-                { tool: "HardHat", proeffiency: 100 },
-                { tool: "Anchor", proeffiency: 87 },
+                { tool: "Python", proeffiency: 93 },
+                { tool: "ML", proeffiency: 83 },
+                { tool: "Langchain", proeffiency: 90 },
+                // { tool: "TypeScript", proeffiency: 100 },
             ]
         },
         {
@@ -116,14 +118,14 @@ const SkillsSection = () => {
                 { tool: "React", proeffiency: 100 },
                 { tool: "TypeScript", proeffiency: 95 },
                 { tool: "Javascript", proeffiency: 100 },
-                { tool: "HTML5", proeffiency: 100 },
-                { tool: "CSS3", proeffiency: 100 },
+                { tool: "HTML5 & CSS3", proeffiency: 100 },
+                // { tool: "CSS3", proeffiency: 100 },
                 { tool: "WegGL", proeffiency: 96 },
                 { tool: "Three JS", proeffiency: 95 },
                 { tool: "Babylon JS", proeffiency: 90 },
-                { tool: "Jest", proeffiency: 100 },
-                { tool: "Mocha", proeffiency: 100 },
-                { tool: "Material UI", proeffiency: 100 },
+                // { tool: "Jest", proeffiency: 100 },
+                // { tool: "Mocha", proeffiency: 100 },
+                // { tool: "Material UI", proeffiency: 100 },
                 { tool: "GraphQL", proeffiency: 100 },
             ]
         },
@@ -146,7 +148,17 @@ const SkillsSection = () => {
                 { tool: "Git", proeffiency: 100 },
                 { tool: "Firebase", proeffiency: 100 },
             ]
-        }
+        },
+        // {
+        //     title: "Blockchain",
+        //     image: block_img,
+        //     tools: [
+        //         { tool: "Solidity", proeffiency: 98 },
+        //         { tool: "Rust", proeffiency: 84 },
+        //         { tool: "HardHat", proeffiency: 100 },
+        //         { tool: "Anchor", proeffiency: 87 },
+        //     ]
+        // },
     ]
 
 
