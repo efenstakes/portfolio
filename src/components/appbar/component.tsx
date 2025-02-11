d7e20c0a14e006d63969b85f30f63ccc9f33c8c2
Efen Stakes <efenstakes101@gmail.com>
2025-02-11 18:32:38 +0300

    update portfolio links

--- src/components/appbar/component.tsx
+++ src/components/appbar/component.tsx
@@ -1,8 +1,8 @@
 import { IconButton } from '@mui/material'
 
 import GitHubIcon from '@mui/icons-material/GitHub'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import EmailIcon from '@mui/icons-material/Email'
+import { FaLinkedin, FaMedium } from "react-icons/fa6";
 
 import './component.scss'
 
@@ -18,21 +18,28 @@ const Appbar = () => {
         <div className="appbar_right row ma_end ca_center">
 
             {/* git */}
-            <a href='https://github.com/efenstakes' className="su_6" style={{ margin: '0 .5rem' }} target='_blank'>
+            <a href='https://github.com/efenstakes' title="My Github Link" className="su_6" style={{ margin: '0 .5rem' }} target='_blank'>
                 <IconButton color='primary' aria-label="github">
                     <GitHubIcon />
                 </IconButton>
             </a>
 
             {/* linkedin */}
-            <a href='https://www.linkedin.com/in/felix-ndunda-0ba841108/' className="su_9" style={{ margin: '0 .5rem' }} target='_blank'>
+            <a href='https://www.linkedin.com/in/felix-ndunda-0ba841108/' title="Medium personal blog" className="su_9" style={{ margin: '0 .5rem' }} target='_blank'>
                 <IconButton color='primary' aria-label="linkedin">
-                    <LinkedInIcon />
+                    <FaLinkedin />
+                </IconButton>
+            </a>
+
+            {/* medium */}
+            <a href='https://medium.com/@efenstakes101' title="Medium personal blog" className="su_9" style={{ margin: '0 .5rem' }} target='_blank'>
+                <IconButton color='primary' aria-label="linkedin">
+                    <FaMedium />
                 </IconButton>
             </a>
 
             {/* email me */}
-            <a href='mailto:efenstakes101@gmail.com' className="su_11" style={{ margin: '0 .5rem' }}>
+            <a href='mailto:efenstakes101@gmail.com' title="Email me" className="su_11" style={{ margin: '0 .5rem' }}>
                 <IconButton color='primary' aria-label="email me">
                     <EmailIcon />
                 </IconButton>
